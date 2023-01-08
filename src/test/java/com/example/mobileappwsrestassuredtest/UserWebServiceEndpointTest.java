17eb7dc386b4a2f1e0df2395d50eab365c801411
cristiii <cristian.vlad10@gmail.com>
2023-01-08 10:31:59 +0200

    create getUserDetails test

--- src/test/java/com/example/mobileappwsrestassuredtest/UserWebServiceEndpointTest.java
+++ src/test/java/com/example/mobileappwsrestassuredtest/UserWebServiceEndpointTest.java
@@ -3,8 +3,7 @@ package com.example.mobileappwsrestassuredtest;
 import io.restassured.RestAssured;
 import io.restassured.response.Response;
 import io.restassured.response.ValidatableResponse;
-import org.junit.jupiter.api.BeforeEach;
-import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.*;
 
 import java.util.HashMap;
 import java.util.Map;
@@ -12,10 +11,13 @@ import java.util.Map;
 import static io.restassured.RestAssured.given;
 import static org.junit.jupiter.api.Assertions.assertNotNull;
 
+@TestMethodOrder(MethodOrderer.OrderAnnotation.class)
 class UserWebServiceEndpointTest {
 
     private static final String EMAIL_ADDRESS = "valid@email.com";
     private static final String APPLICATION_JSON = "application/json";
+    private static String authorization;
+    private static String userId;
 
     @BeforeEach
     void setUp() {
@@ -24,6 +26,7 @@ class UserWebServiceEndpointTest {
     }
 
     @Test
+    @Order(1)
     final void testLoginUser() {
         Map<String, String> loginDetails = new HashMap<>();
         loginDetails.put("email", EMAIL_ADDRESS);
@@ -34,10 +37,27 @@ class UserWebServiceEndpointTest {
                 .post("/users/login")
                 .then().statusCode(200).extract().response();
 
-        String authorization = response.header("Authorization");
-        String userId = response.header("UserID");
+        authorization = response.header("Authorization");
+        userId = response.header("UserID");
 
         assertNotNull(authorization);
         assertNotNull(userId);
+    }
+
+    @Test
+    @Order(2)
+    final void testGetUserDetails() {
+        Response response = given().header("Authorization", authorization).accept(APPLICATION_JSON).when()
+                .get("/users/" + userId)
+                .then()
+                .statusCode(200)
+                .contentType(APPLICATION_JSON)
+                .extract().response();
+
+        String userPublicId = response.jsonPath().getString("userId");
+        String userEmail = response.jsonPath().getString("email");
+
+        assertNotNull(userPublicId);
+        assertNotNull(userEmail);
     }
 }
